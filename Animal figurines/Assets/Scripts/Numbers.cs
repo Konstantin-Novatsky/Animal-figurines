02d749adb8f81ec10b9f7db92864c6c68253b83b
Konstantin <superninja302@yandex.ru>
2023-05-07 00:48:46 +1000

    Version 0.1.1

--- Animal figurines/Assets/Scripts/Numbers.cs
+++ Animal figurines/Assets/Scripts/Numbers.cs
@@ -8,37 +8,92 @@ public class Numbers : MonoBehaviour
 {
     [SerializeField] TMP_Text[] numbers;
     [SerializeField] Button[] enterNumber;
+    [SerializeField] Image[] imageNumbers;
+    [SerializeField] GameObject[] ObjectImageNumber;
+    [SerializeField] Sprite[] spriteForImgNum;
 
+
+    bool[] green;
+    bool[] orange;
+    bool[] red;
+
+    int b = 0;
     int a = 0;
     System.Random random = new System.Random();
     int[] randomNumbers;
 
     private void Start()
     {
+        green = new bool[numbers.Length];
+        orange = new bool[numbers.Length];
+        red = new bool[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++) ObjectImageNumber[i].SetActive(false);
         randomizer();
     }
 
-    public void buttonEnterNumber(int i)
+    public void buttonNumber(int i)
     {
-        if (a < numbers.Length)
+        if (b != numbers.Length)
         {
-            numbers[a].text = Convert.ToString(i);
-            a++;
+            numbers[a].text = Convert.ToString(i); b++;
         }
+        if (a != numbers.Length - 1)
+        {
+            if (green[a] == true) a += 2;
+            else a++;
+        }
+        Debug.Log(b);
+        Debug.Log(a);
     }
 
     public void backNumber()
     {
-        if (a > 0)
+        if (b != -1)
         {
-            a--;
-            numbers[a].text = Convert.ToString("");
+            numbers[a].text = ""; b--;
         }
+        if (green[a] == true && a != 1) a -= 2;
+        else if (a != 0) a--;
+        Debug.Log(b);
+        Debug.Log(a);
     }
 
     public void enterNumbers()
     {
-        
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (randomNumbers[i] == Int32.Parse(numbers[i].text)) green[i] = true;
+            else if (green[i] == false)
+            {
+                for (int a = 0; a < numbers.Length; a++)
+                    if (randomNumbers[a] == Int32.Parse(numbers[i].text)) orange[i] = true;
+            }
+            else red[i] = true;
+        }
+        changeImage();
+    }
+
+    private void changeImage()
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (green[i] == true)
+            {
+                imageNumbers[i].sprite = spriteForImgNum[0];
+                ObjectImageNumber[i].SetActive(true);
+            }
+            else if (orange[i] == true)
+            {
+                imageNumbers[i].sprite = spriteForImgNum[1];
+                ObjectImageNumber[i].SetActive(true);
+            }
+        }
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (green[i] == false && orange[i] == false)
+                numbers[i].text = "";
+        }
+        a = 0;
     }
 
     private void randomizer()
@@ -46,23 +101,6 @@ public class Numbers : MonoBehaviour
         randomNumbers = new int[numbers.Length];
         for (int i = 0; i < numbers.Length; i++)
             randomNumbers[i] = random.Next(0, 10);
-
-    }
-
-    private void comparisons()
-    {
-
-        for (int i = 0; i < numbers.Length * 2; i++)
-        {
-            bool[] green = new bool[numbers.Length];
-            bool[] orange = new bool[numbers.Length];
-            bool[] red = new bool[numbers.Length];
-            if (randomNumbers[i] == Int32.Parse(numbers[i].text)) green[i] = true;
-
-
-        }
-
-
     }
 
 }
